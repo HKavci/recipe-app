39e3b756f31df6c310c1672bc6ef40574bbaabe5
HakanKavci <hackankavci0@gmail.com>
2023-03-03 08:27:20 +0300

    starter

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+
+
+const Home = () => {
+    const BASE_URL = "https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}"
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
